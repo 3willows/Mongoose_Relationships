0b67a460dd8e7b63b946c64c4487cfd770bdebaf
Jasper Wong <wlcjasper@gmail.com>
2024-06-15 22:16:58 +0800

    Add farn and product files

--- Models/farm.js
+++ Models/farm.js
@@ -1,72 +1,37 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
-
-mongoose
-  .connect("mongodb://localhost:27017/relationshipDemo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MONGO CONNECTION OPEN!!!")
-  })
-  .catch((err) => {
-    console.log("OH NO MONGO CONNECTION ERROR!!!!")
-    console.log(err)
-  })
-
-const productSchema = new Schema({
-  name: String,
-  price: Number,
-  season: {
-    type: String,
-    enum: ["Spring", "Summer", "Fall", "Winter"],
-  },
-})
+const mongoose = require('mongoose');
+const Product = require('./product');
+const { Schema } = mongoose;
 
 const farmSchema = new Schema({
-  name: String,
-  city: String,
-  products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+    name: {
+        type: String,
+        required: [true, 'Farm must have a name!']
+    },
+    city: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, 'Email required']
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ]
+});
+
+// DELETE ALL ASSOCIATED PRODUCTS AFTER A FARM IS DELETED
+farmSchema.post('findOneAndDelete', async function (farm) {
+    if (farm.products.length) {
+        const res = await Product.deleteMany({ _id: { $in: farm.products } })
+        console.log(res);
+    }
 })
 
-const Product = mongoose.model("Product", productSchema)
-const Farm = mongoose.model("Farm", farmSchema)
+const Farm = mongoose.model('Farm', farmSchema);
 
-// Product.insertMany([
-//     { name: 'Goddess Melon', price: 4.99, season: 'Summer' },
-//     { name: 'Sugar Baby Watermelon', price: 4.99, season: 'Summer' },
-//     { name: 'Asparagus', price: 3.99, season: 'Spring' },
-// ])
 
-const makeFarm = async () => {
-  const farm = new Farm({ name: "Full Belly Farms", city: "Guinda, CA" })
-  const melon = await Product.findOne({ name: "Goddess Melon" })
-  farm.products.push(melon)
-  await farm.save()
-  console.log(farm)
-}
 
-const addProduct = async () => {
-  const farm = await Farm.findOne({ name: "Full Belly Farms" })
-  const watermelon = await Product.findOne({ name: "Sugar Baby Watermelon" })
-  farm.products.push(watermelon)
-  await farm.save()
-  console.log(farm)
-}
-
-const main = async () => {
-  Farm.find({ name: "Full Belly Farms" }, function (err, data) {
-    if (data.length === 0) {
-      ;(async () => {
-        await makeFarm()
-        await addProduct()
-      })()
-    }
-  })
-  const farm = await Farm.findOne({ name: "Full Belly Farms" }).populate(
-    "products"
-  )
-  console.log(farm)
-}
-
-main()
+module.exports = Farm;
