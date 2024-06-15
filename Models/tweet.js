a3295f8f32cc9227d8dc9276a22b6a6d7079ccda
Jasper Wong <wlcjasper@gmail.com>
2024-06-15 21:40:25 +0800

    Tinkering to ensure the sensible result after dropping all data

--- Models/tweet.js
+++ Models/tweet.js
@@ -1,42 +1,61 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const mongoose = require("mongoose")
+const { Schema } = mongoose
 
-mongoose.connect('mongodb://localhost:27017/relationshipDemo', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!")
-    })
-    .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!!")
-        console.log(err)
-    })
+mongoose
+  .connect("mongodb://localhost:27017/relationshipDemo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!")
+  })
+  .catch((err) => {
+    console.log("OH NO MONGO CONNECTION ERROR!!!!")
+    console.log(err)
+  })
 
 const userSchema = new Schema({
-    username: String,
-    age: Number
+  username: String,
+  age: Number,
 })
 
 const tweetSchema = new Schema({
-    text: String,
-    likes: Number,
-    user: { type: Schema.Types.ObjectId, ref: 'User' }
+  text: String,
+  likes: Number,
+  user: { type: Schema.Types.ObjectId, ref: "User" },
 })
 
-const User = mongoose.model('User', userSchema);
-const Tweet = mongoose.model('Tweet', tweetSchema);
+const User = mongoose.model("User", userSchema)
+const Tweet = mongoose.model("Tweet", tweetSchema)
 
-// const makeTweets = async () => {
-//     // const user = new User({ username: 'chickenfan99', age: 61 });
-//     const user = await User.findOne({ username: 'chickenfan99' })
-//     const tweet2 = new Tweet({ text: 'bock bock bock my chickens make noises', likes: 1239 });
-//     tweet2.user = user;
-//     tweet2.save();
-// }
-
-// makeTweets();
-
-const findTweet = async () => {
-    const t = await Tweet.find({}).populate('user')
-    console.log(t);
+const makeTweets = async () => {
+  let user
+  user = await User.findOne({ username: "chickenfan99" }, function (err, data) {
+    if (!data) {
+      ;(async () => {
+        user = new User({
+          username: "chickenfan99",
+          age: 61,
+        })
+        user.save()
+      })()
+    }
+  })
+  if (user) {
+    user.save()
+  }
+  const tweet2 = new Tweet({ text: "balala text", likes: 1239 })
+  tweet2.user = user
+  tweet2.save()
 }
 
-findTweet();
+makeTweets()
+
+const findTweet = async () => {
+  const t = await Tweet.find().populate("user")
+  console.log(t)
+}
+
+;(async () => {
+  findTweet()
+})()
